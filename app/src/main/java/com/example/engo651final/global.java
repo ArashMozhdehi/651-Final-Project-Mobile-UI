12e298ed8ac7b41bb55fd5d17eb7dd9ba6a69ee3
Arash Mozhdehi <armozhdehi@gmail.com>
2022-04-26 16:26:25 -0600

    Debugging the global class.

--- app/src/main/java/com/example/engo651final/global.java
+++ app/src/main/java/com/example/engo651final/global.java
@@ -6,6 +6,6 @@ public class global extends Application {
     public String l_name = "";
     public String user_name = "";
     public String token = "";
-    public String ip_port = "http://10.12.172.216:5000";
+    public String ip_port = "http://10.80.1.229:5000";
 //    public String ip_port = "https://engo651-arash-mozhdehi.herokuapp.com/";
 }
